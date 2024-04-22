9cd6a6a325d0a60dd400769f1f0ec97e8b1f5d1a
Pablo Acuaviva Huertos <pablo.acuavivahuerto@cpusrv100.scidom.de>
2024-04-22 09:18:02 +0200

    ignore

---
+++ configs/celeba256/add_glasses/cond/test.py
@@ -0,0 +1,29 @@
+from configs.base_uotfm import get_uotfm_config
+from configs.celeba256.add_glasses.base_glasses import get_glasses_config
+from configs.celeba256.base_celeba import get_celeba_config
+from configs.celeba256.base_unet import get_unet_config
+
+
+def get_config():
+    config = get_uotfm_config()
+    config = get_unet_config(config)
+    config = get_celeba_config(config)
+    config = get_glasses_config(config)
+
+    config.training.tau_a = 0.95
+    config.training.tau_b = 0.95
+
+    # config.overfit_to_one_batch = True
+    config.name = "celeba256-add-glasses-FiLM"
+    config.wandb_group = "cond"
+    config.training.num_steps = 500
+    config.training.cond = True
+    
+    # Remove vae & Make fast load with fake data 
+    config.model.use_vae = False 
+    config.data.source = "celeba_fake"
+    config.data.target = "celeba_fake"
+
+    # config.training.batch_size = 256
+
+    return config
